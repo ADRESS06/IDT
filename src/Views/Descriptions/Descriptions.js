3de28759d19ee87b67e5bc39d031581946edabdb
ADRESS <yoimarj0660@gmail.com>
2023-06-20 10:33:11 -0500

    movil adapter

--- src/Views/Descriptions/Descriptions.js
+++ src/Views/Descriptions/Descriptions.js
@@ -15,20 +15,21 @@ const Descriptions = () => {
   return (
     <div className='descriptions' id='descriptions'>
         <motion.h2 initial={{ x: '-50vw' }} animate={{ x: 0 }} transition={{ ease: "easeOut", duration: 0.5 }} >Descriptions</motion.h2>
+
         <motion.div initial={{ y: '100vw' }} animate={{ y: 0 }} transition={{ ease: "easeOut", duration: 1, delay: 0.5 }} className='des'>          
           <Row gutter={16}>
-            <Col span={8}>
-              <Card hoverable style={{ width: 350 }} cover={<img style={{height:'180px'}} alt="example" src={IOT} />}>
+            <Col xs={18} sm={18} md={8} lg={8} xl={8}>
+              <Card className='car' hoverable cover={<img alt="example" src={IOT} />}>
                 <Meta title="IOT" description="Datacenter y nube nativa propia para garantizar la escalabilidad y seguridad de sus aplicaciones IoT" />
               </Card>
             </Col>
-            <Col span={8}>
-            <Card hoverable style={{ width: 350 }} cover={<img style={{height:'180px'}} alt="example" src={RED} />}>
+            <Col xs={18} sm={18} md={8} lg={8} xl={8}>
+            <Card className='car' hoverable cover={<img alt="example" src={RED} />}>
                 <Meta title="Modelos Flexibles De Red" description="Modelos flexibles para desplegar redes privadas empresariales donde y cuando lo necesite" />
               </Card>
             </Col>
-            <Col span={8}>
-            <Card hoverable style={{ width: 350 }} cover={<img style={{height:'180px'}} alt="example" src={SIPROGE} />}>
+            <Col xs={18} sm={18} md={8} lg={8} xl={8}>
+            <Card className='car' hoverable cover={<img alt="example" src={SIPROGE} />}>
                 <Meta title="Sistemas De Proxima Generacion" description="Ofrecemos sistemas en plataformas abiertas que permiten crecer sus aplicaciones en nuestra plataforma de datos" />
               </Card>
             </Col>
